3bce810a857c3dc43f40341320ea7334184a9e4c
egocentryk <lukasz.skowron@gmail.com>
2025-01-31 13:37:21 +0100

    feat(): types expanded

---
+++ src/app/model/user.type.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id: number
+  name: string
+  url: string
+  description: string
+  link: string
+  avatar_url: Avatars
+}
+
+type Avatars = {
+  24: string
+  48: string
+  96: string
+}
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,4 +1,4 @@
-import { computed, effect, inject, Injectable } from '@angular/core'
+import { computed, inject, Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { environment } from '../../environments/environment.development'
 import { rxResource } from '@angular/core/rxjs-interop'
@@ -15,24 +15,16 @@ export class PostsService {
 
   private postResource = rxResource({
     loader: () => {
-      console.log('Before http request')
       return this.http.get<Array<Article>>(this.url).pipe(
         catchError((err) => {
           console.log(err)
           throw err
         }),
-        tap(() => console.log('Before map')),
-        map((post) => post),
-        tap(() => console.log('After map'))
+        map((post) => post)
       )
     },
   })
 
   newPosts = computed(() => this.postResource.value() ?? ([] as Article[]))
   isLoading = this.postResource.isLoading
-
-  loadingEffect = effect(() =>
-    console.log('Loading indicator: ', this.postResource.isLoading())
-  )
-  postsEffect = effect(() => console.log('Post data: ', this.newPosts()))
 }
